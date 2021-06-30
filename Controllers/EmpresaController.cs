ff29914b98ac708007268cdb7e86f6a395356259
Ignacio Quinteros <i.quinteros@hotmail.com>
2021-06-29 22:07:21 -0400

    Init views and controllers displaying

---
+++ Controllers/EmpresaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IgnacioQuinteros.Models;
+
+namespace IgnacioQuinteros.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        // GET: Empresa
+        public ActionResult Index()
+        {
+            var certamen = new certame3Entities2();
+            return View(certamen.Empresas);
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,34 @@
+﻿using IgnacioQuinteros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IgnacioQuinteros.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            var certamen = new certame3Entities2();
+            return View(certamen.Personas);
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+    }
+}
